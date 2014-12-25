9dc1b044a37d71fcea7cdcd2c303b514e3dc3e7d
Carl Walsh <darthwalsh@gmail.com>
2014-12-25 11:28:20 -0800

    Working webserver
    Couple API changes to correctly work with array buffers...

--- server/start.js
+++ server/start.js
@@ -25,36 +25,53 @@ function onCreate(info) {
 var serverSocketId;
 function onListen(id, code) {
   if (code < 0) {
-    console.log("Error listening:" + chrome.runtime.lastError.message);
+    console.log("code: " + code + " Error listening:" + chrome.runtime.lastError.message);
     return;
   }
   serverSocketId = id;
   chrome.sockets.tcpServer.onAccept.addListener(onAccept);
+  chrome.sockets.tcpServer.onAcceptError.addListener(function() { 
+    console.log("onAcceptError");
+  });
 }
 
 function onAccept(info) {
-  if (info.socketId != serverSocketId)
+  if (info.socketId != serverSocketId) {
+    console.log("onAccept !=");
     return;
-  
-  /*var response = "HTTP/1.1 200 OK\r\n" + 
-  "Content-Type: text/plain; charset=UTF-8\r\n" + 
-  "\r\n" + 
-  "Hello world!";
-  var bytes = new TextEncoder("utf-8").encode(response);
-  chrome.sockets.tcp.send(info.clientSocketId, bytes,
-    function(resultCode) {
-      console.log("Data sent to new TCP client connection.")
-  });*/
+  }
   
   chrome.sockets.tcp.onReceive.addListener(function(recvInfo) {
-    if (recvInfo.socketId != info.clientSocketId)
+    if (recvInfo.socketId != info.clientSocketId) {
+      console.log("onRecieve !=");
       return;
-    var request = new TextDecoder("utf-8").decode(recvInfo.data);
+    }
+    var request = new TextDecoder("utf-8").decode(new DataView(recvInfo.data));
     var lines = request.split(/[\n\r]+/);
+    console.log(lines[0]);
   });
   
   chrome.sockets.tcp.onReceiveError.addListener(function(errorInfo) {
-    console.log(errorInfo.resultCode);
+    console.log("onReceiveError:" + errorInfo.resultCode);
+    chrome.sockets.tcp.disconnect(info.clientSocketId, function() {
+      console.log("disconnected");
+    })
   });
-  chrome.sockets.tcp.setPaused(false);
+  
+  chrome.sockets.tcp.setPaused(info.clientSocketId, false, function() {
+    console.log("unpaused");
+  });
+
+  var response = "HTTP/1.1 200 OK\r\n" + 
+  "Content-Type: text/plain; charset=UTF-8\r\n" + 
+  "\r\n" + 
+  "Hello world";
+  var bytes = new TextEncoder("utf-8").encode(response);
+  chrome.sockets.tcp.send(info.clientSocketId, bytes.buffer,
+    function(sendInfo) {
+      console.log(sendInfo.bytesSent + " bytes sent to new TCP client connection.");
+      chrome.sockets.tcp.close(info.clientSocketId);
+
+  });
+  
 }
