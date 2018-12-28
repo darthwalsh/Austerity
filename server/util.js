a48a60e86f105f8df2c8b2e6f63e7c6eb7576ab8
Carl Walsh <darthwalsh@gmail.com>
2018-12-27 17:33:08 -0800

    debugging support

--- server/util.js
+++ server/util.js
@@ -1,11 +1,12 @@
 function wrapErrors(fn) {
   return function() {
-    //TODO(NODE) try {
+    try {
       return fn.apply(this, arguments);
-    // } catch (err) {
-    //   console.log(err.stack);
-    //   console.error(err.message);
-    // }
+    } catch (err) {
+      console.log(err.stack);
+      console.error(err.message);
+      throw err;
+    }
   };
 }
 
