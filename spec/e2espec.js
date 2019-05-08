f7f5a8779ae030272839b23ad92c57dcb03e799b
Carl Walsh <darthwalsh@gmail.com>
2019-05-08 00:26:40 -0700

    Improved E2E player strategy

--- spec/e2espec.js
+++ spec/e2espec.js
@@ -3,24 +3,46 @@
 const Lib = require("../cli/lib");
 require("../server/app");
 
+
+/**
+ * @param {string[]} choices
+ */
+async function bigMoney(choices) {
+  const pickOrder = [
+    "Play All Treasures",
+    "Buy: Province",
+    "Buy: Gold",
+    "Buy: Silver",
+    /'s game/,
+    "New Game",
+  ];
+
+  for (const pick of pickOrder) {
+    if (pick instanceof RegExp) {
+      const filtered = choices.filter(c => c.match(pick));
+      if (filtered.length) {
+        expect(filtered.length).toEqual(1);
+        return filtered[0];
+      }
+    } else if (choices.includes(pick)) {
+      return pick;
+    }
+  }
+
+  return choices[0];
+}
+
 describe("e2e", () => {
   it("plays a game", async done => {
     let output = "";
     const playGame = new Promise((res, rej) => {
       const url = "http://localhost:8080";
-      const p1 = new Lib(url,
-        async choices => {
-          if (choices.includes("New Game")) {
-            return "New Game";
-          }
-          return choices[0];
-        },
-        line => {
-          output += line;
-          if (line.includes("GAME OVER!!!")) {
-            res();
-          }
-        });
+      const p1 = new Lib(url, bigMoney, line => {
+        output += line + "\n";
+        if (line.includes("GAME OVER!!!")) {
+          res();
+        }
+      });
       p1.connect("p1");
     });
 
