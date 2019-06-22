745cccb3dccc0a0d5894ee15915b2cd2f807f034
Carl Walsh <darthwalsh@gmail.com>
2019-06-22 13:37:59 -0700

    add a minor edge case check

--- server/player.js
+++ server/player.js
@@ -170,7 +170,7 @@ class Player {
   fromHand(name) {
     const hi = this.hand.map(c => c.name).indexOf(name);
     if (hi === -1) {
-      return null; // TODO should this throw?
+      throw new Error(`Card ${name} not in hand: ${this.hand.map(c => c.name).join(", ")}`);
     }
     const card = this.hand.splice(hi, 1)[0];
     this.sendHand();
